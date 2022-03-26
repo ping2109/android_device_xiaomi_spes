88de66629392008ecdd982f1909607a07ffc822c
Jabiyeff <cebiyevanar@gmail.com>
2022-03-26 12:58:56 +0400

    fix
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,8 +85,8 @@ DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_bengal
-TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_bengal
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_spes
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_spes
 
 # Kernel
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
