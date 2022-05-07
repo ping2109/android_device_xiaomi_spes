2d0eeeba759276f835ca63c6477b2c564ff03b82
Jabiyeff <cebiyevanar@gmail.com>
2022-05-07 20:24:35 +0400

    spes: Build power blobs
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.3.vendor \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_SOONG_NAMESPACES += \
